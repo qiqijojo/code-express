8f615c154405c622d6071f625db818d910304914
qiqijojo <774948439@qq.com>
2016-09-04 13:59:10 +0000

    Update linkAndDisplay.js

--- express_lib/public/linkAndDisplay.js
+++ express_lib/public/linkAndDisplay.js
@@ -1,29 +1,57 @@
-/**
- * Created by wangqi on 16-8-6.
- */
-const express = require("./../express_lib/express_demo");
-$(document).ready(function () {
-    //$input = $("#inputcontent").html;
-    var para = $("p");
-    var input = $("#inputcontent").html;
-    $("#btn").click(function () {
-        input.split("").some((i,))
+'use strict';
 
-    //if($input)
-    //$("p").display = "none";
-    //$input = $("#inputcontent").html;
-    //$choose_one = $("#ch_1").attr();
-   // console.log($input);
-    //console.log($choose_one);
-    // $("#btn").click(function () {
-    //     $.get({
-    //         "localhost:3005/zipcode", finction(output){
-    //             if (!output.success) {
-    //                 $("p").display = "";
-    //             } else {
-    //                 $("#outputcontent").html(output.data);
-    //             }
-    //         }
-    //     });
-    });
+$(document).ready(function () {
+    let arr = [];
+    const inputTag = $("#inputcontent");
+    const outputTag = $("#outputcontent");
+    const errorMsg = $("#errormsg");
+    inputTag.click(clearResult);
+    $("#btn").click(displayOutput);
+
+    function clearResult(){
+        inputTag.val("");
+        errorMsg.html("");
+        outputTag.val("");
+    }
+
+    function historyList(input, result) {
+        let row = '<tr><td>' + input + '</td><td>' + result + '</td></tr>';
+        $("tbody").append(row);
+        arr.push(row);
+        if (arr.length > 10) {
+            $("tbody").find("tr").eq(1).remove();
+            arr.shift();
+        }
+    }
+
+
+    function expressServer(inputContent) {
+        let radio = $(".choose input[name='trans']:checked").val();
+        $.get("/" + radio + "?code=" + inputContent)
+            .done((data, status) => {
+                getData(data,inputContent)
+            })
+    }
+
+    function getData(data,inputContent){
+        if (!data.success) {
+            outputTag.val("");
+            errorMsg.html(data.data);
+        } else {
+            errorMsg.html("");
+            outputTag.val(data.data);
+            historyList(inputContent, data.data);
+        }
+    }
+
+    function displayOutput() {
+        let inputContent = inputTag.val();
+        if ((inputContent === "") || (inputContent === null)) {
+            errorMsg.html("Empty! Please input the relevant code!.");
+        } else {
+            expressServer(inputContent);
+        }
+    }
+
 });
+
